fe67c59c050bf9d1b9c0e914b4f11825000e6f53
DarenKostov <daren.kostov@outlook.com>
2024-03-25 16:06:33 -0700

    1st commit

---
+++ src/mainDraw.cxx
@@ -0,0 +1,36 @@
+/*
+Copyright (c) 2024 Daren Kostov
+
+This file is part of connect-them-dots
+
+connect-them-dots is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+connect-them-dots is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with connect-them-dots
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "mainClass.hxx"
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Mouse.hpp>
+
+void MainClass::draw(){
+
+  window.clear();
+  window.setView(mainView);
+  
+  sf::RectangleShape rect(sf::Vector2f(30, 30));
+  rect.setFillColor(sf::Color(255, 255, 255, 255));
+  rect.setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window), mainView));
+  window.draw(rect);
+
+  window.display();
+  
+}
---
+++ src/mainUpdate.cxx
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2024 Daren Kostov
+
+This file is part of connect-them-dots
+
+connect-them-dots is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+connect-them-dots is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with connect-them-dots
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "mainClass.hxx"
+
+
+void MainClass::update(){
+}
